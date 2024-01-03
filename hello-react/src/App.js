58e71f7e28384ddc340abce462b7409cd907c022
minseokKim6823 <alstjr6823@naver.com>
2024-01-03 22:51:01 +0900

    test

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  return(
+    <div>
+      <h1>리액트 안녕</h1>
+      <h2>잘 작동 하니?</h2>
     </div>
   );
 }
-
 export default App;
